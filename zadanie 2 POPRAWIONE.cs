30fb401ad43f61cdf1e627c4e525adb5de628037
Telihu <128226986+Telihu@users.noreply.github.com>
2023-06-09 09:50:58 +0200

    Add files via upload

---
+++ zadanie 2 POPRAWIONE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double pole;
+
+        pole = inputValue();
+        Console.WriteLine("Wpisz pole");
+        double obwód;
+
+        obwód = Math.Sqrt(pole / Math.PI);
+        Console.WriteLine(obwód);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
---
+++ zadanie 3 POPRAWIONE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double w, h;
+
+        Console.WriteLine("Wprowadź wzrost");
+        w = inputValue();
+        Console.WriteLine("Wprowadź wagę");
+        h = inputValue();
+        double bMI;
+
+        bMI = w / Math.Pow(h, 2);
+        Console.WriteLine("Twoja wartość BMI to");
+        Console.WriteLine(bMI);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
